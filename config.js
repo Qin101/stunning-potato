f6ab5235d16f72f86e9722a4618cc0af7e79f7fc
Qin101 <h.qin@cityoflondonschool.org.uk>
2022-06-29 11:37:57 +0100

    1

---
+++ config.js
@@ -0,0 +1,11 @@
+export const nftContractAddress = "0xAd6608036b3ee773d337e0cb1661a4da452Ae0aC"
+export const daoContractAddress = "0xDfB83C0f808d04eD48922c092e26516a02E636BC"
+
+export const nftURI = "ipfs://QmUPFUoTfXBSEeJENeJisAGw4u3exsnsaEVE6ZyojV21GF"
+// export const REACT_APP_ALCHEMY_KEY = "https://mumbai.infura.io/v3/4461e5a029b3409a8ccedff7e9ed6485"
+export const REACT_APP_ALCHEMY_KEY = "https://polygon-mumbai.g.alchemy.com/v2/DFj07WnlhgEKBafzUaOvfGh90703rV7q"
+
+
+
+// NFT deployed to: 0xAd6608036b3ee773d337e0cb1661a4da452Ae0aC
+// dao deployed to: 0xed70d1d30A1AC0Ea87cEf10A6Fec5C310460EFB9
